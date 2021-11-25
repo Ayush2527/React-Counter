aefe2054652fb05407643106f07b72b2b17c5862
Ayush2527 <ayush.stha.gps@gmail.com>
2021-11-25 10:28:28 +0545

    first commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './counter.scss';
+import Counter from './counter';
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Counter />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+
